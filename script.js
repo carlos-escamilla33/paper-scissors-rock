78ef2e6aa9f052da30dbaf018207ab14400e7fbf
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-03-08 00:50:02 -0800

    changed return variable in computerPlay function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const gameOptions = ["rock", "paper", "scissors"]
 function computerPlay() {
     // declare a random index that spans from 0 to the length of the array
     const randomIdx = Math.floor(Math.random() * gameOptions.length);
-    const result = gameOptions[randomIdx];
-    return result;
+    const computerResult = gameOptions[randomIdx];
+    return computerResult;
 }
+
