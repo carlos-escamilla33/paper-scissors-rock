d4d9e6599a98f9275d9cefcdab577b0df91d25ea
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-03-08 01:19:16 -0800

    finished playRound function

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ function computerPlay() {
     return computerResult;
 }
 
+function playRound(playerSelection, computerSelection) {
+    // we want to make the selections both case insensitive
+    // we can lowercase both
+    const playerSelect = playerSelection.toLowerCase();
+    const computerSelect = computerSelection.toLowerCase();
+
+    if (playerSelect === "rock" && computerSelect === "scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelect === "paper" && computerSelect === "rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelect === "scissors" && computerSelect === "paper") {
+        return "You Win! Scissors beats Paper";
+    } else if (playerSelect === "rock" && computerSelect === "paper") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelect === "scissors" && computerSelect === "rock") {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelect === "paper" && computerSelect === "scissors") {
+        return "You Lose! Scissors beats Paper";
+    }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
